efb2838b650093057367a5a4e59d4feabb65819f
vivek <vivekgandharkar@outlook.com>
2021-05-16 10:14:34 +0530

    Created UserRegisterForm.

---
+++ burog_auth/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(
+        widget = forms.TextInput(
+            attrs = {
+                'class': 'username__input form__input', 
+                'placeholder': 'jonsm'
+    }))
+    email = forms.EmailField(
+        required=True, 
+        label='email', 
+        widget = forms.EmailInput(
+            attrs={
+                'class': 'email__input form__input', 
+                'placeholder': 'jon@example.com'
+    }))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'password__input form__input', 
+                'placeholder': '••••••••••'
+    }))
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        self.fields.pop('password2')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1']

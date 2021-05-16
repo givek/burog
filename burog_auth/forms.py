847d848a0c2f11b743557f1d806bae90f7128bc4
vivek <vivekgandharkar@outlook.com>
2021-05-16 21:09:39 +0530

    Created UserDetails , UserAuth forms  and  Auth urls.

--- burog_auth/forms.py
+++ burog_auth/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from .models import Profile
 
 
 class EmailLowerField(forms.EmailField):
@@ -10,25 +12,22 @@ class EmailLowerField(forms.EmailField):
 
 class UserRegisterForm(UserCreationForm):
     username = forms.CharField(
-        widget = forms.TextInput(
-            attrs = {
-                'class': 'username__input form__input', 
-                'placeholder': 'jonsm'
-    }))
+        widget=forms.TextInput(
+            attrs={'class': 'username__input form__input', 'placeholder': 'jonsm'}
+        )
+    )
     email = forms.EmailField(
-        required=True, 
-        label='email', 
-        widget = forms.EmailInput(
-            attrs={
-                'class': 'email__input form__input', 
-                'placeholder': 'jon@example.com'
-    }))
+        required=True,
+        label='email',
+        widget=forms.EmailInput(
+            attrs={'class': 'email__input form__input', 'placeholder': 'jon@example.com',}
+        ),
+    )
     password1 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={
-                'class': 'password__input form__input', 
-                'placeholder': '••••••••••'
-    }))
+            attrs={'class': 'password__input form__input', 'placeholder': '••••••••••'}
+        )
+    )
 
     def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
@@ -37,3 +36,43 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1']
+
+
+class UserDetailsForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'first__input form__input', 'placeholder': 'Jon'}
+        )
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'last__input form__input', 'placeholder': 'Smith'}
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
+
+    class Meta:
+        model = Profile
+        fields = ['image']
+
+
+class UserAuthForm(AuthenticationForm):
+    username = forms.CharField(
+        required=True,
+        label='username',
+        widget=forms.TextInput(
+            attrs={'class': 'username__input form__input', 'placeholder': 'jonsm'}
+        ),
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'class': 'password__input form__input', 'placeholder': '••••••••••'}
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
--- burog_auth/urls.py
+++ burog_auth/urls.py
@@ -1,3 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
-urlpatterns = []
+from .forms import UserAuthForm
+
+urlpatterns = [
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            form_class=UserAuthForm, template_name='burog_auth/login.html'
+        ),
+        name='login',
+    ),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]

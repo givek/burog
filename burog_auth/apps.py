c7740cc3633eaeea5683f39ec3e3b4baeb7ac1f0
vivek <vivekgandharkar@outlook.com>
2021-05-20 20:21:00 +0530

    Created signals for create_profile and save_profile.

--- burog_auth/apps.py
+++ burog_auth/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class BurogAuthConfig(AppConfig):
     name = 'burog_auth'
+
+    def ready(self):
+        super(BurogAuthConfig, self).ready()
+        import burog_auth.signals

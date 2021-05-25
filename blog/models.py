2a02806eb30622b3c170074e8ee6f736d429eb3e
vivek <vivekgandharkar@outlook.com>
2021-05-25 22:35:32 +0530

    Created Post model.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    thumbnail = models.ImageField(default='default.png', upload_to='Thumbnails')
+    content = models.TextField(blank=True, null=True)
+    overview = models.TextField(max_length=460, blank=True, null=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-details', kwargs={'pk': self.pk})

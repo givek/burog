cb374cd62a9c5e61378b304994efcff0e5f00905
vivek <vivekgandharkar@outlook.com>
2021-06-02 20:07:36 +0530

    Added check for image to Profile save method.

--- burog_auth/models.py
+++ burog_auth/models.py
@@ -12,10 +12,10 @@ class Profile(models.Model):
 
     def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
+        if self.image:
+            img = Image.open(self.image.path)
 
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
